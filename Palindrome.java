5f18881495f705cbd17e6006ee3b48f03aebf366
aniketnal <aniketnalegaonkar44@gmail.com>
2025-01-20 16:13:22 +0530

    first program

---
+++ Palindrome.java
@@ -0,0 +1,16 @@
+class Palindrome {
+    public static void main(String[] args) {
+        String s = "mam";
+
+        String reverseString = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reverseString += s.charAt(i);
+        }
+
+        if (s.equals(reverseString)) {
+            System.out.println("The string is a palindrome");
+        } else {
+            System.out.println("The string is not a palindrome");
+        }
+    }
+}
