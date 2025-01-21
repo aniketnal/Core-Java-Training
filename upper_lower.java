b36998b0307d2f20f24713a206a59d5f243f9b9d
aniketnal <aniketnalegaonkar44@gmail.com>
2025-01-21 23:01:26 +0530

    string contains upper and lower case letters

---
+++ upper_lower.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+public class upper_lower{
+    public static void main(String[] args){
+        String str = "dsadsda";
+        char[] c = str.toCharArray();
+        int upper = 0;
+        int lower = 0;
+
+        for(int i = 0; i < c.length; i++){
+            if(c[i] >= 65 && c[i] <= 90){
+                upper++;
+            }
+            else if(c[i] >= 97 && c[i] <= 122){
+                lower++;
+
+            }
+
+        }
+
+        if(upper >0 && lower > 0){
+            System.out.println("The string contains both upper and lower case letters");
+        }
+        else if(upper > 0){
+            System.out.println("The string contains only upper case letters");
+        }
+        else if(lower > 0){
+            System.out.println("The string contains only lower case letters");
+
+        }
+        else{
+            System.out.println("The string does not contain both upper and lower case letters");
+        }
+    }
+}
