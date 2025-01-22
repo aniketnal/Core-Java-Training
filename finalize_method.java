aef47e3cea05a2a41a5f812ccd075295acda5398
aniketnal <aniketnalegaonkar44@gmail.com>
2025-01-22 14:22:52 +0530

    runtime

--- finalize_method.java
+++ finalize_method.java
@@ -4,6 +4,8 @@ public class finalize_method {
     public static void main(String[] args){
         finalize_method f = new finalize_method();
         finalize_method f1 = new finalize_method();
+        System.out.println(f);
+        System.out.println(f1);
         f = null;
         f1 = null;
         System.gc();
---
+++ runtime_gd.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+
+public class runtime_gd {
+    
+    public static void main(String[] args){
+        Runtime r = Runtime.getRuntime();
+        System.out.println(r.totalMemory());
+        System.out.println(r.freeMemory());
+        // r = null;
+        // System.gc();
+        for(int i=0; i<10000; i++){
+            Date d = new Date();
+            d = null;
+
+        }
+        System.out.println(r.freeMemory());
+        r.gc();
+        System.out.println(r.freeMemory());
+        // System.out.println("Main method hu");
+    }
+}
