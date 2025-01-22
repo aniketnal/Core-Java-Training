1de4c9193d99dde3806b763b99e7fe6c0d290ed1
aniketnal <aniketnalegaonkar44@gmail.com>
2025-01-22 16:09:51 +0530

    throws

---
+++ exception_prog3.java
@@ -0,0 +1,10 @@
+public class exception_prog3 {
+    public static void main(String[] args) throws InterruptedException {
+        for(int i=0; i < 100; i++){
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+
+    }
+    
+}
