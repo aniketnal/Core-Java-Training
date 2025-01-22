e75c6cbdcab0594a0b3d96c2e2d0eda1a71929f8
aniketnal <aniketnalegaonkar44@gmail.com>
2025-01-22 16:29:46 +0530

    concate and append methods in java

---
+++ stringplay.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class stringplay{
+    public static void main(String[] args){
+        StringBuffer str = new StringBuffer("software");
+        StringBuffer str1 =  new StringBuffer(" developer");
+        // str.concate(str1); // This method does not exist
+        str.append(str1);
+        System.out.println(str);
+
+        // String firstName = "Pratik";
+        // String lastName = " Shinde";
+
+        // System.out.println(firstName.concat(lastName));
+    }
+}
