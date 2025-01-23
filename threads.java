ebe47d5f063f1655446f9bc8d3b8c6a1a790b94a
aniketnal <aniketnalegaonkar44@gmail.com>
2025-01-23 12:15:58 +0530

    some more arithematics

--- threads.java
+++ threads.java
@@ -4,18 +4,25 @@ public class threads extends Thread {
     public void run(){
         for(int i=0; i<3; i++){
             System.out.println(Thread.currentThread().getPriority()+ " Value "+i);
+        }
+        int num = 10;
+        System.out.println(num + 10);
+        System.out.println(num - 5);
+        System.out.println("------------------------------------------------");
 
+        for(int i = 0; i < 10; i++){
+            System.out.println(5*i);
         }
     }
     public static void main(String[] args){
         threads t1 = new threads();
-        threads t2 = new threads();
+        // threads t2 = new threads();
         System.out.println("Minimum priority of a thread: " + Thread.MIN_PRIORITY);
         System.out.println("Normal priority of a thread: " + Thread.NORM_PRIORITY);
         System.out.println("Mamximun priority of a thread: " + Thread.MAX_PRIORITY);
-        t1.setPriority(5);
+        t1.setPriority(1);
         t1.start();
-        t2.setPriority(2);
-        t2.start();      
+        // t2.setPriority(2);
+        // t2.start();      
     }
 }
