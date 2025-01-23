f3188960863a42cdfe4eb8dccc47cff2b950900b
aniketnal <aniketnalegaonkar44@gmail.com>
2025-01-23 16:00:58 +0530

    sortedset in java

--- arraylist.java
+++ arraylist.java
@@ -1,5 +1,8 @@
 // import java.util.ArrayList;
-import java.util.Vector;
+// import java.util.Vector;
+// import java.util.LinkedHashSet;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class arraylist {
     public static void main(String[] args){
@@ -10,16 +13,39 @@ public class arraylist {
         // arr.add(3.14);
         // arr.add(4.2f);
         // arr.add(true);
-        Vector<Integer> arr = new Vector<>();
-        arr.add(1);
+        // Vector<Integer> arr = new Vector<>();
+        // arr.add(1);
+        // arr.add(2);
+        // arr.add(3);
+        // arr.add(4);
+
+        // for(int i = 0; i < arr.size(); i++){
+        //     System.out.println(arr.get(i));
+
+        // }
+
+        // LinkedHashSet<Integer> arr = new LinkedHashSet<>();
+        // arr.add(1);
+        // arr.add(2);
+        // arr.add(3);
+        // arr.add(4);
+        // arr.add(4);
+
+        // for (Integer num : arr) {
+        //     System.out.println(num);
+        // }
+        
+        SortedSet<Integer> arr = new TreeSet<>();
         arr.add(2);
+        arr.add(1);
+        arr.add(4);
         arr.add(3);
         arr.add(4);
 
-        for(int i = 0; i < arr.size(); i++){
-            System.out.println(arr.get(i));
-
+        for (Integer num : arr) {
+            System.out.println(num);
         }
 
     }
+    
 }
