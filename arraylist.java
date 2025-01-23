546054127933ba63005406e3f05a0437ea883896
aniketnal <aniketnalegaonkar44@gmail.com>
2025-01-23 15:23:39 +0530

    vector in java

--- arraylist.java
+++ arraylist.java
@@ -1,14 +1,20 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;
+import java.util.Vector;
 
 public class arraylist {
     public static void main(String[] args){
         // deafault parameter of the arraylist in java id object but it is a good sractice to specify it as it avoids type issue (runtime error)
-        ArrayList<Object> arr = new ArrayList<>();
-        arr.add("mohit");
-        arr.add('a');
-        arr.add(100);
-        arr.add(true);
-        arr.add(10.2f);
+        // ArrayList<Object> arr = new ArrayList<>();
+        // arr.add("moyan");
+        // arr.add(2);
+        // arr.add(3.14);
+        // arr.add(4.2f);
+        // arr.add(true);
+        Vector<Integer> arr = new Vector<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
 
         for(int i = 0; i < arr.size(); i++){
             System.out.println(arr.get(i));
