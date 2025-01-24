e6e89486ca668749879eb514bd711d9909e06d21
aniketnal <aniketnalegaonkar44@gmail.com>
2025-01-24 11:47:31 +0530

    sleep threads while printing

--- run1.java
+++ run1.java
@@ -8,9 +8,14 @@ public class run1 implements Runnable{
         // Runnable run = () -> System.out.println("Hello");
         Runnable run = () -> {for(int i =1; i<=10; i++){
             System.out.println(5*i);
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
         }};
         Thread t1 = new Thread(run);
-
         t1.start();
+        
     }
 }
